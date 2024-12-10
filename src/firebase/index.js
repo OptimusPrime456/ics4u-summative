53c7aaec86037a7c97d922b636860bd501de5171
Steven Wang <steven.wang10@student.tdsb.on.ca>
2024-12-10 11:30:47 -0500

    Added firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyAp2eEWGuOwkNNfvBTaTmiYwJWkcEkOsYU",
+    authDomain: "summative-78780.firebaseapp.com",
+    projectId: "summative-78780",
+    storageBucket: "summative-78780.firebasestorage.app",
+    messagingSenderId: "320774265328",
+    appId: "1:320774265328:web:d0c59399506bdd86bbe535"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
